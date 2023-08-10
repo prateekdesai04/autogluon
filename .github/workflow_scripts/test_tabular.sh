c7f766c83a4507de21095cb12c60d983e8597dcf
EC2 Default User <ec2-user@ip-172-31-10-49.us-west-2.compute.internal>
2023-08-10 17:23:56 +0000

    disabling output capture

--- .github/workflow_scripts/test_tabular.sh
+++ .github/workflow_scripts/test_tabular.sh
@@ -32,7 +32,7 @@ fi
 cd tabular/
 if [ -n "$ADDITIONAL_TEST_ARGS" ]
 then
-    python -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
+    python -m pytest -s -v --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
 else
-    python -m pytest --junitxml=results.xml --runslow tests
+    python -m pytest -s -v --junitxml=results.xml --runslow tests
 fi
