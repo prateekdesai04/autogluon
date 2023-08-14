2f32fa99bcd91ab1bf27ff01fbe5ab8293e593cc
EC2 Default User <ec2-user@ip-172-31-10-49.us-west-2.compute.internal>
2023-08-14 19:02:55 +0000

    removing slow functions

--- tabular/tests/unittests/test_tabular.py
+++ tabular/tests/unittests/test_tabular.py
@@ -698,78 +698,78 @@ def test_pseudolabeling():
                     assert False, error_msg + "unlabeled test data, best quality" + error_suffix
 
 
-@pytest.mark.slow
-def test_tabularHPObagstack():
-    ############ Benchmark options you can set: ########################
-    perf_threshold = 1.1  # How much worse can performance on each dataset be vs previous performance without warning
-    seed_val = 10000  # random seed
-    subsample_size = None
-    hyperparameter_tune_kwargs = {"scheduler": "local", "searcher": "auto"}
-    num_stack_levels = 2
-    num_bag_folds = 2
-    verbosity = 2  # how much output to print
-    hyperparameters = None
-    time_limit = None
-    fast_benchmark = True  # False
-    # If True, run a faster benchmark (subsample training sets, less epochs, etc),
-    # otherwise we run full benchmark with default AutoGluon settings.
-    # performance_value warnings are disabled when fast_benchmark = True.
+# @pytest.mark.slow
+# def test_tabularHPObagstack():
+#     ############ Benchmark options you can set: ########################
+#     perf_threshold = 1.1  # How much worse can performance on each dataset be vs previous performance without warning
+#     seed_val = 10000  # random seed
+#     subsample_size = None
+#     hyperparameter_tune_kwargs = {"scheduler": "local", "searcher": "auto"}
+#     num_stack_levels = 2
+#     num_bag_folds = 2
+#     verbosity = 2  # how much output to print
+#     hyperparameters = None
+#     time_limit = None
+#     fast_benchmark = True  # False
+#     # If True, run a faster benchmark (subsample training sets, less epochs, etc),
+#     # otherwise we run full benchmark with default AutoGluon settings.
+#     # performance_value warnings are disabled when fast_benchmark = True.
 
-    #### If fast_benchmark = True, can control model training time here. Only used if fast_benchmark=True ####
-    if fast_benchmark:
-        subsample_size = 100
-        nn_options = {"num_epochs": 2, "learning_rate": space.Real(0.001, 0.01)}
-        gbm_options = {"num_boost_round": 20, "learning_rate": space.Real(0.01, 0.1)}
-        hyperparameters = {"GBM": gbm_options, "NN_TORCH": nn_options}
-        time_limit = 50
+#     #### If fast_benchmark = True, can control model training time here. Only used if fast_benchmark=True ####
+#     if fast_benchmark:
+#         subsample_size = 100
+#         nn_options = {"num_epochs": 2, "learning_rate": space.Real(0.001, 0.01)}
+#         gbm_options = {"num_boost_round": 20, "learning_rate": space.Real(0.01, 0.1)}
+#         hyperparameters = {"GBM": gbm_options, "NN_TORCH": nn_options}
+#         time_limit = 50
 
-    fit_args = {
-        "num_bag_folds": num_bag_folds,
-        "num_stack_levels": num_stack_levels,
-        "verbosity": verbosity,
-    }
-    if hyperparameter_tune_kwargs is not None:
-        fit_args["hyperparameter_tune_kwargs"] = hyperparameter_tune_kwargs
-    if hyperparameters is not None:
-        fit_args["hyperparameters"] = hyperparameters
-    if time_limit is not None:
-        fit_args["time_limit"] = time_limit
-        fit_args["num_bag_sets"] = 2
-    ###################################################################
-    run_tabular_benchmarks(fast_benchmark=fast_benchmark, subsample_size=subsample_size, perf_threshold=perf_threshold, seed_val=seed_val, fit_args=fit_args)
+#     fit_args = {
+#         "num_bag_folds": num_bag_folds,
+#         "num_stack_levels": num_stack_levels,
+#         "verbosity": verbosity,
+#     }
+#     if hyperparameter_tune_kwargs is not None:
+#         fit_args["hyperparameter_tune_kwargs"] = hyperparameter_tune_kwargs
+#     if hyperparameters is not None:
+#         fit_args["hyperparameters"] = hyperparameters
+#     if time_limit is not None:
+#         fit_args["time_limit"] = time_limit
+#         fit_args["num_bag_sets"] = 2
+#     ###################################################################
+#     run_tabular_benchmarks(fast_benchmark=fast_benchmark, subsample_size=subsample_size, perf_threshold=perf_threshold, seed_val=seed_val, fit_args=fit_args)
 
 
-def test_tabularHPO():
-    ############ Benchmark options you can set: ########################
-    perf_threshold = 1.1  # How much worse can performance on each dataset be vs previous performance without warning
-    seed_val = 99  # random seed
-    subsample_size = None
-    hyperparameter_tune_kwargs = {"scheduler": "local", "searcher": "auto"}
-    verbosity = 2  # how much output to print
-    hyperparameters = None
-    time_limit = None
-    fast_benchmark = True  # False
-    # If True, run a faster benchmark (subsample training sets, less epochs, etc),
-    # otherwise we run full benchmark with default AutoGluon settings.
-    # performance_value warnings are disabled when fast_benchmark = True.
+# def test_tabularHPO():
+#     ############ Benchmark options you can set: ########################
+#     perf_threshold = 1.1  # How much worse can performance on each dataset be vs previous performance without warning
+#     seed_val = 99  # random seed
+#     subsample_size = None
+#     hyperparameter_tune_kwargs = {"scheduler": "local", "searcher": "auto"}
+#     verbosity = 2  # how much output to print
+#     hyperparameters = None
+#     time_limit = None
+#     fast_benchmark = True  # False
+#     # If True, run a faster benchmark (subsample training sets, less epochs, etc),
+#     # otherwise we run full benchmark with default AutoGluon settings.
+#     # performance_value warnings are disabled when fast_benchmark = True.
 
-    #### If fast_benchmark = True, can control model training time here. Only used if fast_benchmark=True ####
-    if fast_benchmark:
-        subsample_size = 100
-        time_limit = 240
-        hyperparameter_tune_kwargs["num_trials"] = 5
+#     #### If fast_benchmark = True, can control model training time here. Only used if fast_benchmark=True ####
+#     if fast_benchmark:
+#         subsample_size = 100
+#         time_limit = 240
+#         hyperparameter_tune_kwargs["num_trials"] = 5
 
-    fit_args = {
-        "verbosity": verbosity,
-    }
-    if hyperparameter_tune_kwargs is not None:
-        fit_args["hyperparameter_tune_kwargs"] = hyperparameter_tune_kwargs
-    if hyperparameters is not None:
-        fit_args["hyperparameters"] = hyperparameters
-    if time_limit is not None:
-        fit_args["time_limit"] = time_limit
-    ###################################################################
-    run_tabular_benchmarks(fast_benchmark=fast_benchmark, subsample_size=subsample_size, perf_threshold=perf_threshold, seed_val=seed_val, fit_args=fit_args)
+#     fit_args = {
+#         "verbosity": verbosity,
+#     }
+#     if hyperparameter_tune_kwargs is not None:
+#         fit_args["hyperparameter_tune_kwargs"] = hyperparameter_tune_kwargs
+#     if hyperparameters is not None:
+#         fit_args["hyperparameters"] = hyperparameters
+#     if time_limit is not None:
+#         fit_args["time_limit"] = time_limit
+#     ###################################################################
+#     run_tabular_benchmarks(fast_benchmark=fast_benchmark, subsample_size=subsample_size, perf_threshold=perf_threshold, seed_val=seed_val, fit_args=fit_args)
 
 
 @pytest.mark.slow
@@ -1134,23 +1134,23 @@ def test_tabular_raise_on_nonfinite_class_labels():
         assert str(ex_info.value).split()[-1] == str(idx)
 
 
-# def test_tabular_log_to_file():
-#     data_root = "https://autogluon.s3.amazonaws.com/datasets/Inc/"
-#     train_data = TabularDataset(data_root + "train.csv")
-#     train_data = train_data.sample(500)
+def test_tabular_log_to_file():
+    data_root = "https://autogluon.s3.amazonaws.com/datasets/Inc/"
+    train_data = TabularDataset(data_root + "train.csv")
+    train_data = train_data.sample(500)
 
-#     predictor = TabularPredictor(label="class", log_to_file=True).fit(train_data=train_data, hyperparameters={"DUMMY": {}})
-#     log = TabularPredictor.load_log(predictor_path=predictor.path)
-#     assert "TabularPredictor saved." in log[-1]
+    predictor = TabularPredictor(label="class", log_to_file=True).fit(train_data=train_data, hyperparameters={"DUMMY": {}})
+    log = TabularPredictor.load_log(predictor_path=predictor.path)
+    assert "TabularPredictor saved." in log[-1]
 
-#     log_file = os.path.join(".", "temp.log")
-#     predictor = TabularPredictor(label="class", log_to_file=True, log_file_path=log_file).fit(train_data=train_data, hyperparameters={"DUMMY": {}})
-#     log = TabularPredictor.load_log(log_file_path=log_file)
-#     assert "TabularPredictor saved." in log[-1]
-#     os.remove(log_file)
+    log_file = os.path.join(".", "temp.log")
+    predictor = TabularPredictor(label="class", log_to_file=True, log_file_path=log_file).fit(train_data=train_data, hyperparameters={"DUMMY": {}})
+    log = TabularPredictor.load_log(log_file_path=log_file)
+    assert "TabularPredictor saved." in log[-1]
+    os.remove(log_file)
 
-#     with pytest.raises(AssertionError):
-#         TabularPredictor.load_log()
+    with pytest.raises(AssertionError):
+        TabularPredictor.load_log()
 
 end_time = datetime.now()
 print('Duration: {}'.format(end_time - start_time))
