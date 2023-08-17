065ae3c986efeefad7fab830985443a1adec7983
EC2 Default User <ec2-user@ip-172-31-10-49.us-west-2.compute.internal>
2023-08-17 22:56:31 +0000

    adding original file

--- .github/workflow_scripts/test_tabular.sh
+++ .github/workflow_scripts/test_tabular.sh
@@ -28,9 +28,9 @@ fi
 cd tabular/
 if [ -n "$ADDITIONAL_TEST_ARGS" ]
 then
-    pip3 uninstall -y ray
+    # pip3 uninstall -y ray
     python3 -m pytest -s -v --junitxml=results.xml --runslow --durations=10 "$ADDITIONAL_TEST_ARGS" tests
 else
-    pip3 uninstall -y ray
+    # pip3 uninstall -y ray
     python3 -m pytest -s -v --junitxml=results.xml --runslow --durations=10 tests
 fi
