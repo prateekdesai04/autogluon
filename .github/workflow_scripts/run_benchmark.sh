adaf20f83918035b17a91cd00fb338f80568d6d8
Ubuntu <ubuntu@ip-172-31-9-154.us-west-2.compute.internal>
2023-10-13 21:36:33 +0000

    test

--- .github/workflow_scripts/run_benchmark.sh
+++ .github/workflow_scripts/run_benchmark.sh
@@ -14,11 +14,11 @@ source $(dirname "$0")/env_setup.sh
 setup_benchmark_env
 
 /bin/bash CI/bench/generate_bench_config.sh $MODULE $PRESET $BENCHMARK $TIME_LIMIT $BRANCH_OR_PR_NUMBER
-agbench run $MODULE"_cloud_configs.yaml" --wait
 pwd
 echo "Viewing all files"
 echo "Printing Cloud Config File"
 cat $MODULE"_cloud_configs.yaml"
+agbench run $MODULE"_cloud_configs.yaml" --wait
 
 # If it is a PR, fetch the cleaned file of master-evaluation
 if [ $BRANCH_OR_PR_NUMBER != "master" ]
