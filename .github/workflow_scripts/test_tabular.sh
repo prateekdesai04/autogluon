1c76fe1e393f6026f49609443aba7cb780e99e51
Ubuntu <ubuntu@ip-172-31-9-154.us-west-2.compute.internal>
2024-02-05 19:41:59 +0000

    Test CI

--- .github/workflow_scripts/test_tabular.sh
+++ .github/workflow_scripts/test_tabular.sh
@@ -28,7 +28,7 @@ fi
 cd tabular/
 if [ -n "$ADDITIONAL_TEST_ARGS" ]
 then
-    python -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
+    python -m pytest --junitxml=results.xml --runslow --durations=0 "$ADDITIONAL_TEST_ARGS" tests
 else
-    python -m pytest --junitxml=results.xml --runslow tests
+    python -m pytest --junitxml=results.xml --runslow --durations=0 tests
 fi
