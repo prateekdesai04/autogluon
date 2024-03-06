4a9884245d71578193ad04851460a7c8c1be783f
Ubuntu <ubuntu@ip-172-31-9-154.us-west-2.compute.internal>
2024-03-06 23:35:36 +0000

    test

--- .github/workflow_scripts/version_diff.sh
+++ .github/workflow_scripts/version_diff.sh
@@ -54,6 +54,7 @@ elif [ $diff_exit_code -eq 1 ]; then
             echo "| <pre>$prev</pre> | <pre>$current</pre> |" >> table_output.txt
         fi
     done < ./diff_output.txt
+    echo "Test!!"
     cat ./table_output.txt
 else
     echo "Error: diff command failed with exit code $diff_exit_code"
