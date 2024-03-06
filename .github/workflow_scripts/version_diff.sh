eb392994b3830c3660a61d2f29e17a7cfd672925
Ubuntu <ubuntu@ip-172-31-9-154.us-west-2.compute.internal>
2024-03-06 23:07:41 +0000

    test

--- .github/workflow_scripts/version_diff.sh
+++ .github/workflow_scripts/version_diff.sh
@@ -34,7 +34,17 @@ if [ $diff_exit_code -eq 0 ]; then
     echo "No difference"
 elif [ $diff_exit_code -eq 1 ]; then
     echo "\nPackage Differences Below:\n"
-    cat ./diff_output.txt
+    echo "| Previous | Current |" > table_output.txt
+    echo "| --- | --- |" >> table_output.txt
+    while IFS= read -r line; do
+        if [[ $line == \<* ]]; then
+            current=$line
+        elif [[ $line == \>* ]]; then
+            prev=$line
+            echo "| $prev | $current |" >> table_output.txt
+        fi
+    done < ./diff_output.txt
+    cat ./table_output.txt
 else
     echo "Error: diff command failed with exit code $diff_exit_code"
     exit 1
