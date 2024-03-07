5f796315fcf7637a0274dd6542c294491f1e9545
Ubuntu <ubuntu@ip-172-31-9-154.us-west-2.compute.internal>
2024-03-07 19:49:52 +0000

    Test

--- .github/workflow_scripts/version_diff.sh
+++ .github/workflow_scripts/version_diff.sh
@@ -58,10 +58,10 @@ elif [ $diff_exit_code -eq 1 ]; then
 
     # Create table
     echo "| Previous | Current |" > table_output.txt
-    echo "| --- | --- |" >> table_output.txt
+    echo "| :---: | :---: |" >> table_output.txt
     for key in "${!prev_packages[@]}" "${!curr_packages[@]}"; do
-        prev="< ${key}=${prev_packages[$key]}"
-        curr="< ${key}=${curr_packages[$key]}"
+        curr="< ${key}=${prev_packages[$key]}"
+        prev="< ${key}=${curr_packages[$key]}"
         if [[ -z ${prev_packages[$key]} ]]; then
         prev="-"
         fi
